b5fd1c7139a079ea56f44428cd07bdabdbfd84f1
cybersekh <carolina.jpeg@gmail.com>
2022-10-24 16:32:03 -0300

    work in progress

---
+++ src/pages/Recipes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from '../components/SearchBar';
+import Login from '../components/Login';
+
+export default function Page() {
+  return (
+    <div>
+      <Login />
+      <SearchBar />
+    </div>
+  );
+}
