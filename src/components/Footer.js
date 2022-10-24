f699563f18768bff2bc6c20945c5c6ab7b0d3bfc
Lucas Victor <lucassvtf@gmail.com>
2022-10-24 20:06:55 -0300

    Requisito 17 e 18

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
-function Footer() {
+export default function Footer() {
   const history = useHistory();
+
+  const styles = {
+    position: 'fixed',
+    bottom: '0px',
+  };
+
   return (
-    <footer data-testid="footer">
+    <footer
+      data-testid="footer"
+      style={ styles }
+    >
       <input
         type="image"
-        src="../images/drinkIcon.svg"
+        src={ drinkIcon }
         alt="Icone para a página de bebidas"
         data-testid="drinks-bottom-btn"
         onClick={ () => history.push('/drinks') }
       />
       <input
         type="image"
-        src="../images/mealIcon.svg"
+        src={ mealIcon }
         alt="Icone para a página de comidas"
         data-testid="meals-bottom-btn"
         onClick={ () => history.push('/meals') }
@@ -22,5 +33,3 @@ function Footer() {
     </footer>
   );
 }
-
-export default Footer;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <h1>Página não encontrada</h1>
+  );
+}
